0b58d5ad4241bd4ed3b175e72447dc016ece2c35
Tom Fraczek <35508031+tomfraczek@users.noreply.github.com>
2018-01-22 16:11:24 +0100

    Add files via upload

---
+++ js/googlemap.js
@@ -0,0 +1,11 @@
+function initMap() {
+        var uluru = {lat: 52.234, lng: 18.213};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 14,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
